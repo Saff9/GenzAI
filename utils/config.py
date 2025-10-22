a948d9f828642fee5bfaa75747a5ffcd77232689
Saff9 <saffanakbar942@gmail.com>
2025-10-23 04:58:28 +0530

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_API_KEY = os.getenv("")
 CLAUDE_API_KEY = os.getenv("CLAUDE_API_KEY")
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = os.getenv("https://edgdiwssxqgnayydwahc.supabase.co")
+SUPABASE_KEY = os.getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkZ2Rpd3NzeHFnbmF5eWR3YWhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjExNTc1MzgsImV4cCI6MjA3NjczMzUzOH0.lDNxoJ1hkw01QAhStAyqJlCEPxwpkm09Wr8mWl4QeuU")
